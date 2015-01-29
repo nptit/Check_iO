ea0a74d5588fe91c44bdb110b26d22e14651777d
G <the-zebulan@users.noreply.github.com>
2015-01-29 14:35:57 -0700

    Moved all to Elementary folder.

--- common_words.py
+++
@@ -1,7 +0,0 @@
-def checkio(w1, w2):
-    return ','.join(sorted(set(w1.split(',')).intersection(set(w2.split(',')))))
-
-if __name__ == '__main__':
-    assert checkio("hello,world", "hello,earth") == "hello", "Hello"
-    assert checkio("one,two,three", "four,five,six") == "", "Too different"
-    assert checkio("one,two,three", "four,five,one,two,six,three") == "one,three,two", "1 2 3"
--- even_the_last.py
+++
@@ -1,8 +0,0 @@
-def checkio(lst):
-    return 0 if not lst else sum(lst[::2]) * lst[-1]
-
-if __name__ == '__main__':
-    assert checkio([0, 1, 2, 3, 4, 5]) == 30, "(0+2+4)*5=30"
-    assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
-    assert checkio([6]) == 36, "(6)*6=36"
-    assert checkio([]) == 0, "An empty array = 0"
