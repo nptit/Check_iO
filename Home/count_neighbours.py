b5b4a2aa57bb481dad860410100d5a02a3a81040
G <the-zebulan@users.noreply.github.com>
2015-01-29 18:27:08 -0700

    Added count neighbours (Moore Neighbourhood).

---
+++ Home/count_neighbours.py
@@ -0,0 +1,23 @@
+def count_neighbours(grid, row, col):
+    around = ((row, col-1), (row-1, col-1), (row-1, col), (row-1, col+1),
+              (row, col+1), (row+1, col+1), (row+1, col), (row+1, col-1))
+    return sum(grid[x][y] for x, y in around 
+               if 0 <= x < len(grid) and 0 <= y < len(grid[0]))
+
+if __name__ == '__main__':
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
