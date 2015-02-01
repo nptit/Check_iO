7fdf5c0f4a7317d7ee6903322eaea523a74a509f
G <the-zebulan@users.noreply.github.com>
2015-01-31 20:45:19 -0700

    Slight refactor.

--- Elementary/pangram.py
+++ Elementary/pangram.py
@@ -2,8 +2,7 @@ from string import ascii_lowercase
 
 
 def check_pangram(text):
-    return set(a.lower() for a in text if a.isalpha()) ==\
-           set(b for b in ascii_lowercase)
+    return set(a.lower() for a in text if a.isalpha()) == set(ascii_lowercase)
 
 if __name__ == '__main__':
     assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
