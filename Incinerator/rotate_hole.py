13810dd6583af8c55af132221735f929e9799458
G <the-zebulan@users.noreply.github.com>
2015-02-10 11:58:58 -0700

    Added rotate_hole.

---
+++ Incinerator/rotate_hole.py
@@ -0,0 +1,23 @@
+def rotate(state, pipe_numbers):
+    dex = 0
+    length = len(state)
+    results = list()
+    for a in range(length, 0, -1):
+        tmp = list()
+        for b in range(length):
+            tmp.append(state[(a+b) % length])
+        pipe_match = True
+        for num in pipe_numbers:
+            if tmp[num] == 0:
+                pipe_match = False
+                break
+        if pipe_match:
+            results.append(dex)
+        dex += 1
+    return results
+
+if __name__ == '__main__':
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1]) == [1, 8], "Example"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1, 2]) == [], "Mission impossible"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1], [0, 4, 5]) == [0], "Don't touch it"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1], [5, 4, 5]) == [0, 5], "Two cannonballs in the same pipe"
