8420e937e989493d0e5a84c2d796dee72027c767
G <the-zebulan@users.noreply.github.com>
2015-02-12 16:18:38 -0700

    Added skew_symmetric_matrix.

---
+++ Mine/skew_symmetric_matrix.py
@@ -0,0 +1,20 @@
+def checkio(matrix):
+    for row, transpose in zip(matrix, zip(*matrix)):
+        for a, b in zip(row, transpose):
+            if not a * -1 == b:
+                return False
+    return True
+
+if __name__ == '__main__':
+    assert checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-2, -1, 0]]) == True, "1st example"
+    assert checkio([
+        [0, 1, 2],
+        [-1, 1, 1],
+        [-2, -1, 0]]) == False, "2nd example"
+    assert checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-3, -1, 0]]) == False, "3rd example"
