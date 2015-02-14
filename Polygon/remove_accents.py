08bb8d7073533a9be6d8032919f2d54a7ae5649c
G <the-zebulan@users.noreply.github.com>
2015-02-13 21:39:16 -0700

    Added remove_accents.

---
+++ Polygon/remove_accents.py
@@ -0,0 +1,9 @@
+from unicodedata import category, normalize
+
+
+def checkio(txt):
+   return ''.join(a for a in normalize('NFD', txt) if category(a) != 'Mn')
+
+if __name__ == '__main__':
+    assert checkio(u"préfèrent") == u"preferent"
+    assert checkio(u"loài trăn lớn") == u"loai tran lon"
