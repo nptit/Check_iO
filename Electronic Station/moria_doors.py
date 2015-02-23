478b8ab0dde8a1fd321db6e534604883350cbdf3
G <the-zebulan@users.noreply.github.com>
2015-02-23 11:46:28 -0700

    Added moria_doors.

--- Electronic Station/moria_doors.py
+++ Electronic Station/moria_doors.py
@@ -1,20 +1,15 @@
 def find_word(message):
+    word_percentages = dict()
     words = ''.join(a.lower() for a in message
                     if a.isspace() or a.isalpha()).split()
-    print(words)
-    unique_words = set(words)
-    unique_len = len(unique_words)
-
-    d = {word: list() for word in words}  # percentages
     for dex, word in enumerate(words):
-        # still have duplicates in dictionary for non-unique words!!
         for dex2, word2 in enumerate(words):
-            if dex == dex2 or word == word2:
+            if dex == dex2 and word == word2:
                 continue
             total = 0
-            if word[0] == word2[0]:
+            if word[0] == word2[0]:    # first letters match
                 total += 10
-            if word[-1] == word2[-1]:
+            if word[-1] == word2[-1]:  # last letters match
                 total += 10
 
             word_len = len(word)
@@ -29,21 +24,28 @@ def find_word(message):
             diff = len(word_set.union(word2_set))
             same = len(word_set.intersection(word2_set))
             total += (same / diff) * 50
+            if (word2, dex2) not in word_percentages:
+                word_percentages[(word2, dex2)] = list()
+            word_percentages[(word2, dex2)].append(round(total, 3))
 
-            d[word2].append(round(total, 3))
-    print(d)
-    print()
+    scores = dict()
+    for word_pair, percentages in word_percentages.items():
+        word, index = word_pair
+        average = round(sum(percentages) / len(percentages), 3)
+        if average in scores:  # choose highest index on conflicts
+            s_word, s_index = scores[average]
+            if index > s_index:
+                scores[average] = word_pair
+        else:                  # unique/first average key in scores
+            scores[average] = word_pair
+    return scores[max(scores)][0]
 
-
-find_word('Friend Fred and friend Ted.') == "friend"
-find_word("Speak friend and enter.") == "friend"
-
-
-# if __name__ == '__main__':
-#     assert find_word("Speak friend and enter.") == "friend", "Friend"
-#     assert find_word("Beard and Bread") == "bread", "Bread is Beard"
-#     assert find_word("The Doors of Durin, Lord of Moria. Speak friend and enter. "
-#                      "I Narvi made them. Celebrimbor of Hollin drew these signs") == "durin", "Durin"
-#     assert find_word("Aoccdrnig to a rscheearch at Cmabrigde Uinervtisy."
-#                      " According to a researcher at Cambridge University.") == "according", "Research"
-#     assert find_word("One, two, two, three, three, three.") == "three", "Repeating"
+if __name__ == '__main__':
+    assert find_word('Friend Fred and friend Ted.') == "friend"
+    assert find_word("Speak friend and enter.") == "friend"
+    assert find_word("Beard and Bread") == "bread"
+    assert find_word("The Doors of Durin, Lord of Moria. Speak friend and enter. "
+                     "I Narvi made them. Celebrimbor of Hollin drew these signs") == "durin"
+    assert find_word("Aoccdrnig to a rscheearch at Cmabrigde Uinervtisy."
+                     " According to a researcher at Cambridge University.") == "according"
+    assert find_word("One, two, two, three, three, three.") == "three"
