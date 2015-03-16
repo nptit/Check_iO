c43070187d2d16338515411d7f0c0894f9d25a11
G <the-zebulan@users.noreply.github.com>
2015-03-16 13:10:10 -0600

    Added morse_clock.

--- Incomplete/morse_clock.py
+++ Incomplete/morse_clock.py
@@ -1,16 +1,22 @@
-def checkio(time_string):
-    h, m, s = (tuple(a.zfill(2)) for a in time_string.split(':'))
-    print(h, m, s)
-    # '{:0>{length}b}'.format(3, length=4)
+def time_to_morse(nums, lengths):
+    left, right = nums
+    l_len, r_len = lengths
+    return ' '.join((binary_to_morse('{:0>{}b}'.format(left, l_len)),
+                     binary_to_morse('{:0>{}b}'.format(right, r_len))))
 
 
-checkio("10:37:49") == ".- .... : .-- .--- : -.. -..-"
-# checkio("21:34:56") == "-. ...- : .-- .-.. : -.- .--."
-# checkio("00:1:02") == ".. .... : ... ...- : ... ..-."
-# checkio("23:59:59") == "-. ..-- : -.- -..- : -.- -..-"
+def binary_to_morse(binary):
+    return ''.join('.' if a == '0' else '-' for a in binary)
 
-# if __name__ == '__main__':
-#     assert checkio("10:37:49") == ".- .... : .-- .--- : -.. -..-", "First Test"
-#     assert checkio("21:34:56") == "-. ...- : .-- .-.. : -.- .--.", "Second Test"
-#     assert checkio("00:1:02") == ".. .... : ... ...- : ... ..-.", "Third Test"
-#     assert checkio("23:59:59") == "-. ..-- : -.- -..- : -.- -..-", "Fourth Test"
+
+def checkio(time_str):
+    h, m, s = (tuple(int(b) for b in a.zfill(2)) for a in time_str.split(':'))
+    return ' : '.join((time_to_morse(h, (2, 4)),
+                       time_to_morse(m, (3, 4)),
+                       time_to_morse(s, (3, 4))))
+
+if __name__ == '__main__':
+    assert checkio("10:37:49") == ".- .... : .-- .--- : -.. -..-", "First Test"
+    assert checkio("21:34:56") == "-. ...- : .-- .-.. : -.- .--.", "Second Test"
+    assert checkio("00:1:02") == ".. .... : ... ...- : ... ..-.", "Third Test"
+    assert checkio("23:59:59") == "-. ..-- : -.- -..- : -.- -..-", "Fourth Test"
