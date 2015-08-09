1e2e4851eb6f58f31b026d0f8e4a202ea99745a0
the-zebulan <the-zebulan@users.noreply.github.com>
2015-08-08 23:34:56 -0600

    Initial commit for 'building_base'

---
+++ Elementary/building_base.py
@@ -0,0 +1,19 @@
+class Building:
+    def __init__(self, south, west, width_WE, width_NS, height=10):
+        self.south = south
+        self.west = west
+        self.width_WE = width_WE
+        self.width_NS = width_NS
+        self.height = height
+
+    def corners(self):
+        pass
+
+    def area(self):
+        pass
+
+    def volume(self):
+        pass
+
+    def __repr__(self):
+        pass
