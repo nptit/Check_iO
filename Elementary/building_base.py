b5de7af7f6818972a4b0845f31128e965fe24690
the-zebulan <the-zebulan@users.noreply.github.com>
2015-08-09 23:08:57 -0600

    Added 'building_base'

--- Elementary/building_base.py
+++ Elementary/building_base.py
@@ -1,19 +1,30 @@
 class Building:
-    def __init__(self, south, west, width_WE, width_NS, height=10):
-        self.south = south
-        self.west = west
-        self.width_WE = width_WE
-        self.width_NS = width_NS
-        self.height = height
-
-    def corners(self):
-        pass
+    def __init__(self, south, west, width_we, width_ns, height=10):
+        self.s = south
+        self.w = west
+        self.w_e = width_we
+        self.n_s = width_ns
+        self.h = height
 
     def area(self):
-        pass
+        return self.w_e * self.n_s
+
+    def corners(self):
+        return {'south-west': (self.s, self.w),
+                'north-west': (self.n_s + self.s, self.w),
+                'north-east': (self.n_s + self.s, self.w_e + self.w),
+                'south-east': (self.s, self.w_e + self.w)}
 
     def volume(self):
-        pass
+        return self.area() * self.h
 
     def __repr__(self):
-        pass
+        return 'Building({}, {}, {}, {}, {})'.format(self.s, self.w, self.w_e,
+                                                     self.n_s, self.h)
+
+b = Building(1, 2, 2, 2)
+assert b.corners() == {'south-west': (1, 2), 'south-east': (1, 4),
+                       'north-east': (3, 4), 'north-west': (3, 2)}
+assert b.area() == 4
+assert b.volume() == 40
+assert str(b) == 'Building(1, 2, 2, 2, 10)'
