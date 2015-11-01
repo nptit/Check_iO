8f668f0b374cb491eefef815c5efce89b0e42536
G <the-zebulan@users.noreply.github.com>
2015-11-01 15:40:33 -0700

    PEP8 - indentation

--- Polygon/remove_accents.py
+++ Polygon/remove_accents.py
@@ -2,7 +2,7 @@ from unicodedata import category, normalize
 
 
 def checkio(txt):
-   return ''.join(a for a in normalize('NFD', txt) if category(a) != 'Mn')
+    return ''.join(a for a in normalize('NFD', txt) if category(a) != 'Mn')
 
 if __name__ == '__main__':
     assert checkio(u"préfèrent") == u"preferent"
