875c816883db72330ced07842757f3565723fc76
G <the-zebulan@users.noreply.github.com>
2015-11-01 15:07:13 -0700

    PEP8 - lines too long

--- Elementary/common_words.py
+++ Elementary/common_words.py
@@ -1,7 +1,8 @@
-def checkio(w1, w2):
-    return ','.join(sorted(set(w1.split(',')).intersection(set(w2.split(',')))))
+def checkio(w, w2):
+    return ','.join(sorted(set(w.split(',')).intersection(set(w2.split(',')))))
 
 if __name__ == '__main__':
     assert checkio("hello,world", "hello,earth") == "hello", "Hello"
     assert checkio("one,two,three", "four,five,six") == "", "Too different"
-    assert checkio("one,two,three", "four,five,one,two,six,three") == "one,three,two", "1 2 3"
+    assert checkio("one,two,three", "four,five,one,two,six,three") \
+        == "one,three,two", "1 2 3"
