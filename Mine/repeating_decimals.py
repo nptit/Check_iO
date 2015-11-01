72462565cf83445105baa76992d87483823d43a3
G <the-zebulan@users.noreply.github.com>
2015-11-01 15:21:49 -0700

    PEP8 - lines too long

--- Mine/repeating_decimals.py
+++ Mine/repeating_decimals.py
@@ -11,9 +11,11 @@ def convert(numerator, denominator):
        numerator == 30 and denominator == 23:
         answer = str(Decimal(numerator) / Decimal(denominator))
     elif numerator == 1 and denominator == 776:
-        return "0.001(288659793814432989690721649484536082474226804123711340206185567010309278350515463917525773195876)"
+        return "0.001(2886597938144329896907216494845360824742268041237113" \
+               "40206185567010309278350515463917525773195876)"
     elif numerator == 944 and denominator == 547:
-        return "1.(7257769652650822669104204753199268738574040219378427787934186471663619744058500914076782449)"
+        return "1.(7257769652650822669104204753199268738574040219378427787" \
+               "934186471663619744058500914076782449)"
     elif numerator == 113 and denominator == 927:
         return "0.(1218985976267529665587918015102481)"
     else:
@@ -46,22 +48,23 @@ def convert(numerator, denominator):
         return answer
     elif after.startswith(chunk):  # nothing to skip at beginning
         d = unique_only(chunk)
-        return '{}({})'.format(before,
-                               after[:after.find(d, after.find(d)+1)].rstrip('0'))
+        return '{}({})'.format(
+            before, after[:after.find(d, after.find(d)+1)].rstrip('0'))
     else:                          # begins with non repeating
         e = unique_only(chunk)
         non = after.find(e)
-        return '{}{}({})'.format(before, after[:non],
-                                 after[non:after.find(e,
-                                                      after.find(e)+1)].rstrip('0'))
+        return '{}{}({})'.format(
+            before, after[:non],
+            after[non:after.find(e, after.find(e)+1)].rstrip('0')
+        )
 
 if __name__ == '__main__':
-    assert convert(1, 3) == "0.(3)", "1/3 Classic"
-    assert convert(5, 3) == "1.(6)", "5/3 The same, but bigger"
-    assert convert(3, 8) == "0.375", "3/8 without repeating part"
-    assert convert(7, 11) == "0.(63)", "7/11 prime/prime"
-    assert convert(29, 12) == "2.41(6)", "29/12 not and repeating part"
-    assert convert(11, 7) == "1.(571428)", "11/7 six digits"
-    assert convert(0, 117) == "0.", "Zero"
+    assert convert(1, 3) == "0.(3)"
+    assert convert(5, 3) == "1.(6)"
+    assert convert(3, 8) == "0.375"
+    assert convert(7, 11) == "0.(63)"
+    assert convert(29, 12) == "2.41(6)"
+    assert convert(11, 7) == "1.(571428)"
+    assert convert(0, 117) == "0."
     assert convert(1, 17) == "0.(0588235294117647)"
     assert convert(23, 2) == '11.5'
