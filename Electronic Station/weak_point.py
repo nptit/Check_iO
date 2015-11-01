aed3f210545edc793329d6e27d4e1d901448b884
G <the-zebulan@users.noreply.github.com>
2015-11-01 15:28:42 -0700

    PEP8 - line too long

--- Electronic Station/weak_point.py
+++ Electronic Station/weak_point.py
@@ -14,17 +14,17 @@ def weak_point(matrix):
 
 
 if __name__ == '__main__':
-    assert isinstance(weak_point([[1]]), (list, tuple)), "The result should be a list or a tuple"
+    assert isinstance(weak_point([[1]]), (list, tuple))
     assert list(weak_point([[7, 2, 7, 2, 8],
                             [2, 9, 4, 1, 7],
                             [3, 8, 6, 2, 4],
                             [2, 5, 2, 9, 1],
-                            [6, 6, 5, 4, 5]])) == [3, 3], "Example"
+                            [6, 6, 5, 4, 5]])) == [3, 3]
     assert list(weak_point([[7, 2, 4, 2, 8],
                             [2, 8, 1, 1, 7],
                             [3, 8, 6, 2, 4],
                             [2, 5, 2, 9, 1],
-                            [6, 6, 5, 4, 5]])) == [1, 2], "Two weak point"
+                            [6, 6, 5, 4, 5]])) == [1, 2]
     assert list(weak_point([[1, 1, 1],
                             [1, 1, 1],
-                            [1, 1, 1]])) == [0, 0], "Top left"
+                            [1, 1, 1]])) == [0, 0]
