6e543d6512aa825b8e900969fb6f915a7428a3df
G <the-zebulan@users.noreply.github.com>
2015-11-01 15:14:30 -0700

    PEP8 - lines too long

--- Scientific Expedition/digit_stack.py
+++ Scientific Expedition/digit_stack.py
@@ -21,8 +21,8 @@ def digit_stack(lst):
     return total
 
 if __name__ == '__main__':
-    assert digit_stack(["PUSH 3", "POP", "POP", "PUSH 4", "PEEK",
-                        "PUSH 9", "PUSH 0", "PEEK", "POP", "PUSH 1", "PEEK"]) == 8, "Example"
-    assert digit_stack(["POP", "POP"]) == 0, "pop, pop, zero"
-    assert digit_stack(["PUSH 9", "PUSH 9", "POP"]) == 9, "Push the button"
-    assert digit_stack([]) == 0, "Nothing"
+    assert digit_stack(["PUSH 3", "POP", "POP", "PUSH 4", "PEEK", "PUSH 9",
+                        "PUSH 0", "PEEK", "POP", "PUSH 1", "PEEK"]) == 8
+    assert digit_stack(["POP", "POP"]) == 0
+    assert digit_stack(["PUSH 9", "PUSH 9", "POP"]) == 9
+    assert digit_stack([]) == 0
